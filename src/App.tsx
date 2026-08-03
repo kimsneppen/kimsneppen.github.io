73675f4bbda9633d7cfcc6a79247b280b6c0845c
kimsneppen <ksneppen@gmail.com>
2026-08-03 22:31:58 +0200

    Update title and file reference for BTW Sandpile

--- src/App.tsx
+++ src/App.tsx
@@ -28,10 +28,10 @@ type Model = {
 const MODELS: Model[] = [
   {
   id: "btw-sandpile",
-  title: "BTW Sandpile",
+  title: "BTW Sandpile + Game",
   description:
     "The classical sandpile model of Self Organized Criticality: Office visualization, Variants, Analysis and Games",
-  file: "/models/btw_sandpile_v37.html",
+  file: "/models/btw_sandpile_v39.html",
   image: "/images/btw.png",
   status: "live",
 },  
