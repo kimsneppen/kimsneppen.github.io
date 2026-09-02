cb042427edbe10aa300ba19d7ef8d6e1b5817ad4
kimsneppen <ksneppen@gmail.com>
2026-09-02 18:34:07 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -137,7 +137,7 @@ const MODELS: Model[] = [
     description:
       "A Large dynamical system with deterministic chaos",
     file: "/models/kuramoto.html",
-    thumbnail: "/thumbnails/kuramoto.png",
+    thumbnail: "/thumbnails/kuramoto_2d.png",
   },    
     {
     id: "sneppen-model",
